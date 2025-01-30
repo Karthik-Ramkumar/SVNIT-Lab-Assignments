b76ed5877c15eae21af1d6a6c7912b77458705c1
Karthik-Ramkumar <karthik.sb.ram@gmail.com>
2025-01-30 13:31:16 +0530

    edited 6.py

--- sem-2/WPP/Lab-assignments/lab_1/6.py
+++ sem-2/WPP/Lab-assignments/lab_1/6.py
@@ -1,1 +1,26 @@
-print("hi")
+import math
+
+coords = []
+for i in range(3):
+    coords.append(input("Enter X-Y-Z separated by - "))  
+nearest_neighbors = []
+
+for coord in coords:
+    X, Y, Z = map(int, coord.split("-"))
+    shortest = float('inf')
+    nearest = None
+
+    for coord1 in coords:
+        if coord1 == coord:
+            continue 
+
+        X1, Y1, Z1 = map(int, coord1.split("-"))
+        short = math.sqrt((X - X1) ** 2 + (Y - Y1) ** 2 + (Z - Z1) ** 2)
+
+        if short < shortest:
+            shortest = short
+            nearest = coord1
+
+    nearest_neighbors.append((coord, nearest))
+
+print(nearest_neighbors)
