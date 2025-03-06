3c5b349e2921a5103fa58f755beb937322542afb
Karthik-Ramkumar <karthik.sb.ram@gmail.com>
2025-03-06 19:18:20 +0000

    .

--- sem-2/misc/midsem.py
+++ sem-2/misc/midsem.py
@@ -30,3 +30,61 @@ print(std.name)
 print(std.college_name)
 # or
 print(Student.college_name)
+
+
+
+
+class Car:
+    def __init__(self, type):
+        self.type = type
+
+    @staticmethod
+    def start():
+        print("car started..")
+
+    @staticmethod
+    def stop():
+        print("car stopped.")
+
+class ToyotaCar(Car):
+    def __init__(self, name, type):
+        self.name = name
+        super().__init__(type) # calling parent class's constructor
+
+car1 = ToyotaCar("prius", "electric")
+car1.start()
+
+
+
+# class method
+class car:
+    name = "toyota"
+    def changeName(self, name):
+        self.name = name
+        self.__class__.name = 'honda'  # accessing class from inside method
+        car.name = 'honda'
+    
+    @classmethod
+    def changeName(cls, Name):          # cls is class
+        cls.name = Name  
+
+
+class Car:
+    def __init__(self, brand, speed):
+        self.brand = brand  
+        self.speed = speed
+
+    @property
+    def speed(self):  # Getter
+        return self._speed
+
+    @speed.setter
+    def speed(self, value):  # Setter
+            self._speed = value
+
+car1 = Car("Toyota", 120)
+print(car1.brand)  # Directly accessing public variable
+print(car1.speed)  # Using @property getter
+
+car1.speed = 150  # Using @property setter
+print(car1.speed)
