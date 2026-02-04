75d71b3d835d811a18b764ddd02c73a7f89bd379
Karthik-Ramkumar <karthik.sb.ram@gmail.com>
2026-02-04 12:09:50 +0530

    made changes to lab 3

--- sem-4/AI/3/2.py
+++ sem-4/AI/3/2.py
@@ -1,5 +1,5 @@
 import random
-#Emergency lever overrides,Obstacle detection overrides train movement
+#Emergency lever overrides,Obstacle detection overrides train movement so it is rational
 # percept values
 trainStates = ['detected', 'notDetected']
 obstacleStates = ['clear', 'obstacle']
@@ -7,21 +7,33 @@ emergencyStates = ['neutral', 'active']
 
 # percept (trainDetected, obstaclePresent, emergencyLever)
 # action (gateArm, siren, trainSignal)
-ruleTable = {
-    ('detected', 'obstacle', 'neutral'): ('lower', 'on', 'red'),
-    ('detected', 'obstacle', 'active'):  ('lower', 'on', 'red'),
-    ('detected', 'clear', 'active'):     ('lower', 'on', 'red'),
-    ('detected', 'clear', 'neutral'):    ('lower', 'on', 'green'),
-    ('notDetected', 'obstacle', 'neutral'): ('lower', 'on', 'red'), # RAISE?
-    ('notDetected', 'obstacle', 'active'):  ('lower', 'on', 'red'), # RAISE?
-    ('notDetected', 'clear', 'active'):     ('lower', 'on', 'red'),
-    ('notDetected', 'clear', 'neutral'):    ('raise', 'off', 'green')
-}
+
+# RULE TABLE [train, obstacle, emergency, actionIndex]
+
+ruleTable = [
+ ['detected','obstacle','neutral',0],
+ ['detected','obstacle','active',0],
+ ['detected','clear','active',1],
+ ['detected','clear','neutral',2],
+ ['notDetected','obstacle','neutral',0],    # should we not raise the gate here?
+ ['notDetected','obstacle','active',0],         
+ ['notDetected','clear','active',1],
+ ['notDetected','clear','neutral',3]
+]
+
+#  ACTION TABLE [gate, siren, signal, cost]
+
+actionTable = [
+ ['lower','on','red',5],     # action 0
+ ['lower','on','red',4],     # action 1
+ ['lower','on','green',3],   # action 2
+ ['raise','off','green',1]   # action 3
+]
 
 location = 'levelCrossing'
 
-print("percept (train, obstacle, emergency)\taction (gate, siren, signal)\tlocation")
-print("--------------------------------------------------------------------------")
+print("percept (train, obstacle, emergency)\taction (gate, siren, signal, cost)\tlocation")
+print("-------------------------------------------------------------------------------------")
 
 for step in range(10):
     trainDetected = random.choice(trainStates)
@@ -30,6 +42,12 @@ for step in range(10):
 
     percept = (trainDetected, obstaclePresent, emergencyLever)
 
-    action = ruleTable[percept]
+    actionIndex = None
+    for row in ruleTable:
+        if row[0]==trainDetected and row[1]==obstaclePresent and row[2]==emergencyLever:
+            actionIndex = row[3]
+            break
 
-    print(percept, "\t", action, "\t", location)
+    action = actionTable[actionIndex]
+
+    print(percept,"\t",action,"\t",location)
